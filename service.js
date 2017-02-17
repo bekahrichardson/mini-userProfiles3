ad834143d595007c290da1d8abd562d5e3b92639
Rebekah Richardson <bekahrichardson0422@gmail.com>
2017-02-16 17:55:10 -0700

    add deferred variable

--- service.js
+++ service.js
@@ -1,6 +1,7 @@
 angular.module('userProfiles').service('mainService', function($http, $q) {
 
   this.getUsers = function() {
+    var deferred = $q.defer();
     return $http({
         method: 'GET',
         url: 'http://reqres.in/api/users?page=1'
