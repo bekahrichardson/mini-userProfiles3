3c47c5dc117c27a5a092920866be647a60ed38d2
Rebekah Richardson <bekahrichardson0422@gmail.com>
2017-02-16 17:58:10 -0700

    adding a promise

--- service.js
+++ service.js
@@ -2,10 +2,12 @@ angular.module('userProfiles').service('mainService', function($http, $q) {
 
   this.getUsers = function() {
     var deferred = $q.defer();
-    return $http({
+    $http({
         method: 'GET',
         url: 'http://reqres.in/api/users?page=1'
+    }).then(function(response) {
+      deferred.resolve(response)
     })
+    return deferred.promise;
   }
-
 });
